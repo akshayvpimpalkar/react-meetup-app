a241f5bf26f866d4c3d1cd38a909edf4944557a0
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2021-04-18 20:39:48 +0530

    Added routing and Navigation component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
-      Hello Akshay!!!
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact><AllMeetupsPage /></Route>
+        <Route path='/new-meetup'><NewMeetupPage /></Route>
+        <Route path='/favorites'><FavoritesPage /></Route>
+      </Switch>
     </div>
   );
 }
