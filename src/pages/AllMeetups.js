36958ce28accba20f7e2300bfc2118a68a5fddc5
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2021-04-20 17:30:35 +0530

    building wrapper components with props.children

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -5,7 +5,7 @@ const DUMMY_DATA = [
         id: 'm1',
         title: 'This is a first meetup',
         image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+          'https://imgsv.imaging.nikon.com/lineup/dslr/df/img/sample/img_01_l.jpg',
         address: 'Meetupstreet 5, 12345 Meetup City',
         description:
           'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
